0f4e560b0e5c49db46a5781a892b514695cd0e48
Yasmim Barbosa Vieira <vieiray937@gmail.com>
2022-03-21 23:11:16 -0300

    chore: add menu options mobile

--- src/components/menu-options/menuOptions.tsx
+++ src/components/menu-options/menuOptions.tsx
@@ -1,17 +1,41 @@
 import style from './menuOptions.module.scss';
 import pet from '../../assets/img/shampoo.svg'
+import petBrinquedo from '../../assets/img/brinquedo-de-estimacao.svg';
+import petScratcher from '../../assets/img/scratcher.svg';
+import petGarrafa from '../../assets/img/garrafa-pet.svg';
+import petShop from '../../assets/img/pet-shop.svg';
 
 export default function MenuOptions() {
     return (
         <>
             <section className={style.menuOptions}>
-                <header className={style.header}>
+                <header className={style.menuOptions__header}>
                     <h2>Do que seu pet gosta?</h2>
                 </header>
                 <section className={style.menuOptions__itens}>
                     <section className={style.menuOptions__objects}>
                         <figure className={style.menuOptions__images}>
                             <img src={pet} alt="" />
+                        </figure>
+                    </section>
+                    <section className={style.menuOptions__objects}>
+                        <figure className={style.menuOptions__images}>
+                            <img src={petBrinquedo} alt="" />
+                        </figure>
+                    </section>
+                    <section className={style.menuOptions__objects}>
+                        <figure className={style.menuOptions__images}>
+                            <img src={petScratcher} alt="" />
+                        </figure>
+                    </section>
+                    <section className={style.menuOptions__objects}>
+                        <figure className={style.menuOptions__images}>
+                            <img src={petGarrafa} alt="" />
+                        </figure>
+                    </section>
+                    <section className={style.menuOptions__objects}>
+                        <figure className={style.menuOptions__images}>
+                            <img src={petShop} alt="" />
                         </figure>
                     </section>
                 </section>
